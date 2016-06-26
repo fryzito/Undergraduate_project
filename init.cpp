edfcebc3012a68f71bbaa009fe87ac09d7cdb888
fvilca <fvilca@gmail.com>
2016-06-25 19:09:01 -0500

    first commit
    super commit

---
+++ init.cpp
@@ -0,0 +1,25 @@
+string search;
+ifstream inFile;
+string line;
+
+inFile.open(test.html);
+
+if(!inFile){
+cout << "Unable to open file" << endl;
+exit(1);
+}
+cout << "Enter word to search for: ";
+cin >>search;
+
+
+size_t pos;
+while(inFile.good())
+  {
+      getline(inFile,line); // get line from file
+      pos=line.find(search); // search
+      if(pos!=string::npos) // string::npos is returned if string is not found
+        {
+            cout <<"Found!";
+            break;
+        }
+  }
